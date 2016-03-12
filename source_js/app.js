44b9d0d9dd9db76a103bad4b3a458b89a154a3db
Brian Rooney <btroone2@illinois.edu>
2016-03-12 05:50:49 +0000

    mp3

--- source_js/app.js
+++ source_js/app.js
@@ -1,5 +1,11 @@
-var app = angular.module('mp3',['ngRoute']);
 
-app.config(function ($routeProvider) {
+var app = angular.module('mp3', ['ngRoute','movieController',]);
 
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/list', {'templateUrl': 'partials/list.html','controller': 'listController'})
+    .when('/gallery', {'templateUrl': 'partials/gallery.html','controller': 'galleryController'})
+    .when('/details/:rank', {'templateUrl': 'partials/details.html','controller': 'detailController'})
+    .otherwise({'redirectTo': '/gallery'});
 })
+
--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,1 +1,31 @@
 /* Sample Controller */
+
+var movieController = angular.module('movieController', []);
+
+movieController.controller('detailController', ['$scope', '$http', '$routeParams',function($scope, $http, $routeParams) {
+    $http.get('./data/imdb250.json').then(function(response) {
+      var movie = response.data;
+      for (var i in movie) {
+        var m = movie[i];
+        if (m.rank == $routeParams.rank) {
+            $scope.movie = m;
+        }
+      }
+    });
+  },
+]);
+
+movieController.controller('galleryController', ['$scope', '$http', function($scope, $http) {
+  $http.get('./data/imdb250.json').then(function(response) {
+    $scope.movies = response.data;
+    $scope.filters = {};
+  });
+  },
+]);
+
+movieController.controller('listController', ['$scope', '$http', function($scope, $http) {
+    $http.get('./data/imdb250.json').then(function(response) {
+      $scope.movies = response.data
+    });
+  },
+]);
